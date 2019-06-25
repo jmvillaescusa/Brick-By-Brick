fc8b0cbdd211098fabbb0e71588e4b97c9746904
jmv <44001505+jmvillaescusa@users.noreply.github.com>
2019-06-25 18:31:06 -0400

    Start

---
+++ Brick by BrickTests/Brick_by_BrickTests.swift
@@ -0,0 +1,34 @@
+//
+//  Brick_by_BrickTests.swift
+//  Brick by BrickTests
+//
+//  Created by Jaimeson Mario Villaescusa on 2019-06-25.
+//  Copyright © 2019 Jaimeson Mario Villaescusa. All rights reserved.
+//
+
+import XCTest
+@testable import Brick_by_Brick
+
+class Brick_by_BrickTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
