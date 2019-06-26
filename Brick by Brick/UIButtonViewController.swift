6880bad37c51a199bb5d8174a2d223e60098d5d5
jmv <44001505+jmvillaescusa@users.noreply.github.com>
2019-06-25 20:18:04 -0400

    Buttons

---
+++ Brick by Brick/UIButtonViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UIButtonViewController.swift
+//  Brick by Brick
+//
+//  Created by Jaimeson Mario Villaescusa on 2019-06-25.
+//  Copyright © 2019 Jaimeson Mario Villaescusa. All rights reserved.
+//
+
+import UIKit
+
+class UIButtonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
