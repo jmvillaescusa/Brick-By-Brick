b66ecf559f6581d79175c57df6f2e98e2b58a7f0
jmv <44001505+jmvillaescusa@users.noreply.github.com>
2019-06-26 20:15:44 -0400

    Movement and Turning done

--- Brick by Brick/GameScene.swift
+++ Brick by Brick/GameScene.swift
@@ -20,17 +20,24 @@ class GameScene: SKScene {
     //var spawnedBlock: Bool = false
     
     let screenSize: CGRect = UIScreen.main.bounds
-    var spawnLocation = CGPoint(x: 0, y: 0)
+    var spawnLocation = CGPoint(x: 0, y: -10)
     
     //var lArray = SKSpriteNode[100]()
     //struct Array <SKSpriteNode>
     
     var fallingBlock = SKSpriteNode()
+    var fallingSpeed: CGFloat = 5
     var tetrisBlocks = [SKSpriteNode]()
     
     //UI Buttons
-    let rotateLeft = SKSpriteNode(imageNamed: "LetterO")
-    let rotateRight = SKSpriteNode(imageNamed: "LetterO")
+    let rotateLeft = SKSpriteNode(imageNamed: "left")
+    let rotateRight = SKSpriteNode(imageNamed: "right")
+    let holdPiece = SKSpriteNode(imageNamed: "hold") //Doesn't do anything yet
+    
+    //Movement
+    var movingDown: Bool = false
+    var movingLeft: Bool = false
+    var movingRight: Bool = false
     
     override func didMove(to view: SKView) {
         setupButton()
@@ -43,42 +50,22 @@ class GameScene: SKScene {
     
     func SpawnBlock(){
         spawnLocation = CGPoint(x: screenSize.width * 0.3, y: screenSize.height + 50)
-        var randNum = Int.random(in: 0..<7)
-        if (randNum == 0){
-            letterL()
-        }
         
-        if (randNum == 1){
-            letterI()
-        }
-        
-        if (randNum == 2){
-            letterJ()
-        }
-        
-        if (randNum == 3){
-            letterO()
-        }
-        
-        if (randNum == 4){
-            letterS()
-        }
-        
-        if (randNum == 5){
-            letterT()
-        }
-        
-        if (randNum == 6){
-            letterZ()
-        }
-        //rotateLeft()
+        let randNum = Int.random(in: 0..<7)
+        if (randNum == 0){ letterL() }        
+        if (randNum == 1){ letterI() }
+        if (randNum == 2){ letterJ() }
+        if (randNum == 3){ letterO() }
+        if (randNum == 4){ letterS() }
+        if (randNum == 5){ letterT() }
+        if (randNum == 6){ letterZ() }
     }
     
     func dropBlock(){
         let SpawnBlock = SKAction.run{
             self.SpawnBlock()
         }
-        let repeatNewBlock = SKAction.repeatForever(SKAction.sequence([SpawnBlock, SKAction.wait(forDuration: 2)]))
+        let repeatNewBlock = SKAction.repeatForever(SKAction.sequence([SpawnBlock, SKAction.wait(forDuration: 5)]))
         
         run(repeatNewBlock)
     }
@@ -154,25 +141,30 @@ class GameScene: SKScene {
     }
     
     func turnLeft() {
-        fallingBlock.zRotation = fallingBlock.zRotation + 180
+        fallingBlock.zRotation = fallingBlock.zRotation + CGFloat(90).degreesToRadians
     }
     func turnRight() {
-        fallingBlock.zRotation = fallingBlock.zRotation - 180
+        fallingBlock.zRotation = fallingBlock.zRotation - CGFloat(90).degreesToRadians
     }
     
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        //fallingBlock.position.y -= fallingSpeed
     }
     
     func setupButton() {
-        rotateLeft.position = CGPoint(x: -250, y: -800)
+        rotateLeft.position = CGPoint(x: -250, y: -775)
         rotateLeft.zPosition = 100
         addChild(rotateLeft)
         
-        rotateRight.position = CGPoint(x: 250, y: -800)
+        rotateRight.position = CGPoint(x: 250, y: -775)
         rotateRight.zPosition = 100
         addChild(rotateRight)
+        
+        holdPiece.position = CGPoint(x: 0, y: -775)
+        holdPiece.zPosition = 100
+        addChild(holdPiece)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -183,7 +175,50 @@ class GameScene: SKScene {
                 turnLeft()
             } else if (rotateRight.frame.contains(location)) {
                 turnRight()
+            } else if (holdPiece.frame.contains(location)) {
+                print("piece held")
             }
         }
     }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let newlocation = touch.location(in: self)
+            let previousLocation = touch.previousLocation(in: self)
+            
+            
+            
+            if (!rotateLeft.frame.contains(newlocation) && !rotateRight.frame.contains(newlocation) && !holdPiece.frame.contains(newlocation)) {
+                if (newlocation.y < previousLocation.y - 20) {
+                    movingDown = true
+                }
+                else if (newlocation.x > previousLocation.x) {
+                    movingRight = true
+                } else if (newlocation.x < previousLocation.x) {
+                    movingLeft = true
+                }
+                
+                if (movingDown && !movingRight && !movingLeft) {
+                    //fallingBlock.position.y -= 40
+                }
+                if (movingRight && !movingLeft && !movingDown) {
+                    fallingBlock.position.x += 25
+                }
+                if (movingLeft && !movingRight && !movingDown) {
+                    fallingBlock.position.x -= 25
+                }
+            }
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for _ in touches { fallingSpeed = 5 }
+        movingDown = false
+        movingLeft = false
+        movingRight = false
+    }
+}
+
+extension CGFloat {
+    var degreesToRadians: CGFloat { return CGFloat(self) * .pi / 180 }
 }
